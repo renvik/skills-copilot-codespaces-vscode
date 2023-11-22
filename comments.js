21558c2bd2c1a7e3cd171ffa2f7e6b0120fe6145
Reko-Aleksi Renvik <reko-aleksi.renvik@knowit.fi>
2023-11-22 12:05:33 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create web server application
+
+
+// Import modules
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Comment = require('./models/comment');
+
+
+
